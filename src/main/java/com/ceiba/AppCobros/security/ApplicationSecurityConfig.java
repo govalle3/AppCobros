bd33d8c43b930fc9630d515cad40c4535f2ea67d
govalle3 <39679249+govalle3@users.noreply.github.com>
2021-05-03 21:27:08 -0500

    Setting authorities to debtors

--- src/main/java/com/ceiba/AppCobros/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ceiba/AppCobros/security/ApplicationSecurityConfig.java
@@ -50,19 +50,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(DEBTOR.name()) // DEBTOR_ROLE
+//                .roles(DEBTOR.name()) // DEBTOR_ROLE
+                .authorities(DEBTOR.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
