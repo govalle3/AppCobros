54e66d86d5e13f71022834e6644a592e9be35809
govalle3 <39679249+govalle3@users.noreply.github.com>
2021-05-06 21:14:15 -0500

    FakeApplicationUserDaoService

---
+++ src/main/java/com/ceiba/AppCobros/auth/ApplicationUserDao.java
@@ -0,0 +1,8 @@
+package com.ceiba.AppCobros.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+}
---
+++ src/main/java/com/ceiba/AppCobros/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,63 @@
+package com.ceiba.AppCobros.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.ceiba.AppCobros.security.ApplicationUserRole.*;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService implements ApplicationUserDao{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                    new ApplicationUser(
+                            DEBTOR.getGrantedAuthorities(),
+                            "annasmith",
+                            passwordEncoder.encode("password"),
+                            true,
+                            true,
+                            true,
+                            true
+                    ),
+                new ApplicationUser(
+                        ADMIN.getGrantedAuthorities(),
+                        "linda",
+                        passwordEncoder.encode("password"),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        ADMINTRAINEE.getGrantedAuthorities(),
+                        "tom",
+                        passwordEncoder.encode("password"),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+        return applicationUsers;
+    }
+}
